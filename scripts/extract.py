0fc936793d86e64a912896c0106e921a4b619ba6
anpabeltj <patrapembinaan@gmail.com>
2026-04-22 15:22:40 +0800

    feat: add extraction script to fetch exchange rates

---
+++ scripts/extract.py
@@ -0,0 +1,17 @@
+import requests
+
+
+
+
+def extract_currency():
+    url = "https://api.frankfurter.dev/v2/rates?from=2026-01-01&quotes=IDR,MYR,SGD,USD&base=USD"
+    response = requests.get(url)
+    return response.json()
+
+if __name__ == "__main__":
+    currency_data = extract_currency()
+
+    for item in currency_data:
+        print(item['base'], item['date'], item['quote'], item['rate'])
+   
+  # Print first 5 results for brevity
